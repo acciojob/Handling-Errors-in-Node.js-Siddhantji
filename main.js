8ed4af6a85d3904547e0f9d1893a996f03a237f6
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-07-22 00:59:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const fs = require('fs');
 
 function printFileContents(filePath) {
-  // TODO: Use fs.readFile to read the file contents
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading file: ${err.message}`);
+      process.exit(1);
+    }
+    console.log(data);
+  });
 }
 
-// TODO: Call printFileContents with the command-line argument
+// Get the file path from command-line arguments
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error("Please provide a file path as a command-line argument.");
+  process.exit(1);
+}
+
+// Call printFileContents with the command-line argument
+printFileContents(filePath);
