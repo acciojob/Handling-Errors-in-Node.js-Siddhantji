a762095f53808421a5b655e409b210c6943de72d
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-07-22 02:07:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
 
-function printFileContents(filePath) {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Column '${columnName}' not found in the CSV.`);
-      process.exit(1);
-    }
-
-    console.log(data);
-  });
-}
-
-// Get the file path from command-line arguments
 const filePath = process.argv[2];
 
 if (!filePath) {
-  console.error("Please provide a file path as a command-line argument.");
+  console.error("Usage: node print.js <file_path>");
   process.exit(1);
 }
 
-// Call printFileContents with the command-line argument
-printFileContents(filePath);
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error(`Error: ${err.message}`);
+  } else {
+    console.log(data);
+  }
+});
